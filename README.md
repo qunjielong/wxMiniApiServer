d80af4ccef423dc5a9dd8a7fa452a082645784eb
Livoras Dai <me@livoras.com>
2020-04-30 18:15:06 +0800

    basically done

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # wxMiniApiServer
 WebView 通过中转服务器调用小程序的接口 wx.XXX
+
+```
+[GIN-debug] POST   /api/apply_token          --> go-web-utilities.Rest.func1 (3 handlers)
+[GIN-debug] POST   /api/tunnels/:tid/set_cache/:key --> go-web-utilities.Rest.func1 (3 handlers)
+[GIN-debug] POST   /api/tunnels/:tid/get_cache --> go-web-utilities.Rest.func1 (3 handlers)
+[GIN-debug] POST   /api/tunnels/:tid/to-h5   --> go-web-utilities.Rest.func1 (3 handlers)
+[GIN-debug] POST   /api/tunnels/:tid/to-mini-app --> go-web-utilities.Rest.func1 (3 handlers)
+[GIN-debug] POST   /api/tunnels/:tid/long-pull/:clientType --> go-web-utilities.Rest.func1 (3 handlers)
+[GIN-debug] POST   /api/tunnels/:tid/heart-beat --> go-web-utilities.Rest.func1 (3 handlers)
+```
--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 )
 
 func main() {
-	log.Println("Hello World")
 	app := gin.Default()
+	trackDyingTunnels()
 	makeApi(app.Group("/api"))
 	err := app.Run(":8080")
 	if err != nil {
@@ -17,7 +17,31 @@ func main() {
 	}
 }
 
+func trackDyingTunnels() {
+	time.AfterFunc(5 * time.Second, func() {
+		for k, t := range tunnels {
+			if t.IsDead {
+				delete(tunnels, k)
+			}
+		}
+		trackDyingTunnels()
+	})
+}
+
 var tunnels = map[string]*Tunnel{}
+
+type TunnelParamHandler func(c *gw.Context, t *Tunnel)
+
+func wrapTunnel(handler TunnelParamHandler) gin.HandlerFunc {
+	return gw.Rest(func(c *gw.Context) {
+		tid := c.C.Param("tid")
+		if t, ok := tunnels[tid]; ok {
+			handler(c, t)
+		} else {
+			c.BadRequest("通信通道不存在或者已经销毁")
+		}
+	})
+}
 
 func makeApi(api *gin.RouterGroup) {
 	api.POST("/apply_token", gw.Rest(func(c *gw.Context) {
@@ -29,32 +53,61 @@ func makeApi(api *gin.RouterGroup) {
 	tunnelApi := api.Group("/tunnels")
 
 	/** 设置缓存 */
-	tunnelApi.POST("/:tid/set_cache", gw.Rest(func(c *gw.Context) {
-		// TODO:
+	tunnelApi.POST("/:tid/set_cache/:key", wrapTunnel(func(c *gw.Context, t *Tunnel) {
+		key := c.C.Param("key")
+		var data interface{}
+		err := c.BindJSON(&data)
+		if err != nil {
+			c.BadRequest(err.Error())
+			return
+		}
+		t.SetCache(key, data)
 	}))
 
 	/** 获取缓存 */
-	tunnelApi.POST("/:tid/get_cache", gw.Rest(func(c *gw.Context) {
-		// TODO:
+	tunnelApi.POST("/:tid/get_cache", wrapTunnel(func(c *gw.Context, t *Tunnel) {
+		key := c.C.Param("key")
+		value := t.GetCache(key)
+		c.OK(value)
 	}))
 
 	/** 小程序发送给 H5 */
-	tunnelApi.POST("/:tid/to-h5", gw.Rest(func(c *gw.Context) {
-		// TODO:
+	tunnelApi.POST("/:tid/to-h5", wrapTunnel(func(c *gw.Context, t *Tunnel) {
+		var data interface{}
+		err := c.BindJSON(&data)
+		if err != nil {
+			c.BadRequest(err.Error())
+			return
+		}
+		t.SendToClient(data)
 	}))
 
 	/** H5 送给小程序 */
-	tunnelApi.POST("/:tid/to-mini-app", gw.Rest(func(c *gw.Context) {
-		// TODO:
+	tunnelApi.POST("/:tid/to-mini-app", wrapTunnel(func(c *gw.Context, t *Tunnel) {
+		var data interface{}
+		err := c.BindJSON(&data)
+		if err != nil {
+			c.BadRequest(err.Error())
+			return
+		}
+		t.SendToHost(data)
 	}))
 
 	/** 长轮训 */
-	tunnelApi.POST("/:tid/long-pull/:clientType", gw.Rest(func(c *gw.Context) {
-
+	tunnelApi.POST("/:tid/long-pull/:clientType", wrapTunnel(func(c *gw.Context, t *Tunnel) {
+		clientType := c.C.Param("clientType")
+		if clientType == "mini" {
+			t.SetHostCtx(c)
+		} else if clientType == "h5" {
+			t.SetClientCtx(c)
+		} else {
+			c.BadRequest("不存在的长轮训类型 " + clientType)
+		}
 	}))
 
 	/** 小程序心跳控制 */
-	tunnelApi.POST("/:tid/heart-beat", gw.Rest(func(c *gw.Context) {
-
+	tunnelApi.POST("/:tid/heart-beat", wrapTunnel(func(c *gw.Context, t *Tunnel) {
+		t.Beat()
 	}))
 }
+
